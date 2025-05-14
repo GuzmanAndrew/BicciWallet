7310f5f4473c207661a4ef7831dc0769549aabd4
Andrew Ramirez <andrew.ramirez@global66.com>
2025-05-14 03:28:03 -0500

    Add enviroment

---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+   production: true,
+   apiUsers: '/api/users'
+   /* apiAccounts: 'http://ms-accounts:8082/api/accounts',
+   apiTransactions: 'http://ms-transactions:8083/api/transactions' */
+};
